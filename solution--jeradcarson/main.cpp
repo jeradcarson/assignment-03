d3575c2c06dd56a5f4afd0270bb28c1c0b9f8d39
Jerad Carson <jerad.carson@gmail.com>
2016-03-12 22:02:00 -0800

    Assignment 3
    I worked with Jenna Martinez and Alfredo Portillo

---
+++ solution--jeradcarson/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+/* Part 1 */
+
+Um
+
+/* Part 2 */
+Template <typename Num> // Template declaration
+
+Num modulusFive (number) { // Function named modulusFive within the Num template
+	number = number%5; // It does exactly what it says on the tin
+	return number;
+}
+
+int main () {
+	int a = 48;
+	float b = 0.05;
+	double c = 22.13;
+
+cout << a << endl << modulusFive(a) << endl;
+cout << b << endl << modulusFive(b) << endl;
+cout << c << endl << modulusFive(c) << endl;
+
+/* Part 3 */
+
+Um
+
+/* Part 4 */
+
+We’ll get around to this at some point I swear
+
+
